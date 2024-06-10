1f154ba6fff49d44e0f9b3dc76237d9d97ef1828
emabondi <emabond69@gmail.com>
2024-06-10 22:24:04 +0200

    added ft_read

---
+++ ft_read.s
@@ -0,0 +1,23 @@
+section	.text
+	extern __errno_location
+	global ft_read
+
+	;args: rdi=fd, rsi=buffer, rdx=count
+
+	ft_read:
+		call __errno_location wrt ..plt
+		mov qword [rax], 0				;sets errno to 0
+		mov rax,0						;1 is syscall number for write
+		syscall
+		test rax, rax					;check return value of syscall
+		js .error						;jump if signed (negative)
+		ret
+
+	.error:
+		neg rax							;fa il negativo per ottenere l'error number
+		push rax						
+		call __errno_location wrt ..plt
+		pop qword [rax]					;push and pop rax in stack to avoid that the call changes it
+		mov rax, -1						;sets ret value
+		ret
+
